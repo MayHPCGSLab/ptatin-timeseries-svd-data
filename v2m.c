b462e2c0633674f0422904a9c10ec7798944035d
Dave May <dave.mayhem23@gmail.com>
2020-10-14 21:12:48 +0200

    Added code to generate the snapshot matrix

---
+++ v2m.c
@@ -0,0 +1,130 @@
+
+/*
+ /Users/dmay/software/petsc-3.13.3/arch-gnu-c-debug/bin/mpicc -Wl,-multiply_defined,suppress -Wl,-multiply_defined -Wl,suppress -Wl,-commons,use_dylibs -Wl,-search_paths_first -Wl,-no_compact_unwind -Wl,-multiply_defined,suppress -Wl,-multiply_defined -Wl,suppress -Wl,-commons,use_dylibs -Wl,-search_paths_first -Wl,-no_compact_unwind  -Wall -Wwrite-strings -Wno-strict-aliasing -Wno-unknown-pragmas -fstack-protector -Qunused-arguments -fvisibility=hidden -g3 -Wall -Wwrite-strings -Wno-strict-aliasing -Wno-unknown-pragmas -fstack-protector -Qunused-arguments -fvisibility=hidden -g3  -I/Users/dmay/software/petsc-3.13.3/include -I/Users/dmay/software/petsc-3.13.3/arch-gnu-c-debug/include -I/opt/X11/include      ex1.c  -Wl,-rpath,/Users/dmay/software/petsc-3.13.3/arch-gnu-c-debug/lib -L/Users/dmay/software/petsc-3.13.3/arch-gnu-c-debug/lib -Wl,-rpath,/Users/dmay/software/petsc-3.13.3/arch-gnu-c-debug/lib -L/Users/dmay/software/petsc-3.13.3/arch-gnu-c-debug/lib -Wl,-rpath,/opt/X11/lib -L/opt/X11/lib -lpetsc -lumfpack -lklu -lcholmod -lbtf -lccolamd -lcolamd -lcamd -lamd -lsuitesparseconfig -lX11 -lmetis -ldl -o ex1
+*/
+
+static char help[] = "Load a collection of PETSc vectors and stack them into a tall-skinny matrix\n\n";
+
+#include <petsc.h>
+#include <petscvec.h>
+#include <petscmat.h>
+
+/* Mesh size (129, 33, 257) <xdmf reverse order>, so ni = 257, nk = 129 */
+PetscErrorCode SnapshotView(Vec u,const char suffix[])
+{
+  const PetscInt M = 257;
+  const PetscInt N = 33;
+  const PetscInt P = 129;
+  DM             dm;
+  Vec            cu;
+  PetscViewer    viewer;
+  char           ofile[PETSC_MAX_PATH_LEN];
+  PetscErrorCode ierr;
+  
+  PetscFunctionBegin;
+  ierr = DMDACreate3d(PETSC_COMM_WORLD,DM_BOUNDARY_NONE,DM_BOUNDARY_NONE,DM_BOUNDARY_NONE,
+                      DMDA_STENCIL_BOX,M,N,P,PETSC_DECIDE,PETSC_DECIDE,PETSC_DECIDE,
+                      1,1,NULL,NULL,NULL,&dm);CHKERRQ(ierr);
+  ierr = DMSetUp(dm);CHKERRQ(ierr);
+  ierr = DMDASetUniformCoordinates(dm,0.0,4.0,-1.0,0.0,0.0,2.0);CHKERRQ(ierr);
+  ierr = DMCreateGlobalVector(dm,&cu);CHKERRQ(ierr);
+  ierr = VecCopy(u,cu);CHKERRQ(ierr);
+  
+  PetscSNPrintf(ofile,PETSC_MAX_PATH_LEN-1,"%s.vts",suffix);
+  ierr = PetscViewerVTKOpen(PETSC_COMM_WORLD,ofile,FILE_MODE_WRITE,&viewer);CHKERRQ(ierr);
+  ierr = VecView(cu,viewer);CHKERRQ(ierr);
+  
+  ierr = PetscViewerDestroy(&viewer);CHKERRQ(ierr);
+  ierr = DMDestroy(&dm);CHKERRQ(ierr);
+  ierr = VecDestroy(&cu);CHKERRQ(ierr);
+  PetscFunctionReturn(0);
+}
+
+PetscErrorCode SnapshotMatCreate(Mat *snapshots)
+{
+  PetscErrorCode  ierr;
+  Vec             u;
+  PetscViewer     viewer;
+  const PetscInt  steps[] = { 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400 };
+  char            fname[PETSC_MAX_PATH_LEN];
+  PetscInt        i,len,m,n,k;
+  Mat             S;
+  PetscInt        *idx;
+
+  PetscFunctionBegin;
+  len = sizeof(steps) / sizeof(PetscInt);
+  n = len;
+  ierr = PetscOptionsGetInt(NULL,NULL,"-truncate",&n,NULL);CHKERRQ(ierr);
+  if (n > len) SETERRQ1(PETSC_COMM_WORLD,PETSC_ERR_USER,"Truncate value cannot be larger than %D",len);
+  
+  for (i=0; i<n; i++) {
+    PetscSNPrintf(fname,PETSC_MAX_PATH_LEN-1,"data/step%1.6d_energy.pbvec",steps[i]);
+    PetscPrintf(PETSC_COMM_WORLD,"[%d] Loading: %s\n",i,fname);
+  
+    ierr = VecCreate(PETSC_COMM_WORLD,&u);CHKERRQ(ierr);
+    
+    ierr = PetscViewerBinaryOpen(PETSC_COMM_WORLD,fname,FILE_MODE_READ,&viewer);CHKERRQ(ierr);
+    ierr = VecLoad(u,viewer);CHKERRQ(ierr);
+    ierr = PetscViewerDestroy(&viewer);CHKERRQ(ierr);
+    
+    if (i == 0) {
+      ierr = VecGetSize(u,&m);CHKERRQ(ierr);
+      printf("S: m %d, n %d\n",m,n);
+      ierr = MatCreate(PETSC_COMM_WORLD,&S);CHKERRQ(ierr);
+      ierr = MatSetSizes(S,PETSC_DECIDE,PETSC_DECIDE,m,n);CHKERRQ(ierr);
+      ierr = MatSetType(S,MATDENSE);CHKERRQ(ierr);
+      ierr = MatSetUp(S);CHKERRQ(ierr);
+      
+      ierr = PetscCalloc1(m,&idx);CHKERRQ(ierr);
+      for (k=0; k<m; k++) { idx[k] = k; }
+    }
+    {
+      const PetscScalar *_u;
+      
+      ierr = VecGetArrayRead(u,&_u);CHKERRQ(ierr);
+      ierr = MatSetValues(S,m,idx,1,&i,_u,INSERT_VALUES);CHKERRQ(ierr);
+      ierr = VecRestoreArrayRead(u,&_u);CHKERRQ(ierr);
+    }
+    ierr = MatAssemblyBegin(S,MAT_FLUSH_ASSEMBLY);CHKERRQ(ierr);
+    ierr = MatAssemblyEnd(S,MAT_FLUSH_ASSEMBLY);CHKERRQ(ierr);
+    
+    if (i == 0) {
+      ierr = SnapshotView(u,"snapshot0");CHKERRQ(ierr);
+    }
+    
+    ierr = VecDestroy(&u);CHKERRQ(ierr);
+  }
+  ierr = MatAssemblyBegin(S,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
+  ierr = MatAssemblyEnd(S,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
+
+  *snapshots = S;
+  
+  ierr = PetscFree(idx);CHKERRQ(ierr);
+  
+  PetscFunctionReturn(0);
+}
+
+int main(int argc,char **args)
+{
+  PetscErrorCode ierr;
+  Mat            S;
+  
+  ierr = PetscInitialize(&argc,&args,(char*)0,help);if (ierr) return ierr;
+  
+  ierr = SnapshotMatCreate(&S);CHKERRQ(ierr);
+
+  /* Dump S(i,j) as ascii to stdout */
+  /*ierr = MatView(S,PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);*/
+  
+  /* Dump S(i,j) as bindary to file */
+  {
+    PetscViewer  viewer;
+    
+    ierr = PetscViewerBinaryOpen(PETSC_COMM_WORLD,"snapshot.pbmat",FILE_MODE_WRITE,&viewer);CHKERRQ(ierr);
+    ierr = MatView(S,viewer);CHKERRQ(ierr);
+    ierr = PetscViewerDestroy(&viewer);CHKERRQ(ierr);
+  }
+  
+  ierr = PetscFinalize();
+  return ierr;
+}
